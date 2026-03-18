c5bdc310308476153079a79cb25ca6da93d1d20c
Guy Gregory <guy.gregory@microsoft.com>
2026-03-18 17:06:13 +0000

    Added priority processing sample

---
+++ python/responses-priority-aoai-v1.py
@@ -0,0 +1,19 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    base_url=os.getenv("AZURE_OPENAI_V1_API_ENDPOINT"),
+)
+
+response = client.responses.create(
+    model=os.environ["AZURE_OPENAI_API_MODEL"],
+    input="Tell me a joke.",
+    service_tier="priority"
+)
+
+print(response.output_text)
+print("Service Tier:", response.service_tier)
